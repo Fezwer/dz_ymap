703af6d7cd16579e98f95db6487c7fea756e22e4
Fezwer <oleg-lib@yandex.ru>
2024-02-02 00:42:28 +1000

    fix 2-6

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 import 'package:geolocator/geolocator.dart';
 
+class PlacemarkMapObject {
+  final MapObjectId mapId;
+  final Point point;
+  final double opacity;
+  final double direction;
+  final bool isDraggable;
+  final PlacemarkIcon icon;
+
+  PlacemarkMapObject({
+    required this.mapId,
+    required this.point,
+    required this.opacity,
+    required this.direction,
+    required this.isDraggable,
+    required this.icon,
+  });
+
+  factory PlacemarkMapObject.fromObject({
+    required MapObjectId mapId,
+    required Point point,
+    required double opacity,
+    required double direction,
+    required bool isDraggable,
+    required PlacemarkIcon icon,
+  }) =>
+      PlacemarkMapObject(
+        mapId: mapId,
+        point: point,
+        opacity: opacity,
+        direction: direction,
+        isDraggable: isDraggable,
+        icon: icon,
+      );
+}
+
 void main() {
   runApp(MyApp());
 }
@@ -26,7 +61,7 @@ class __MapScreenStateState extends State<_MapScreenState> {
   final MapObjectId targetMapObjectId = const MapObjectId('target_placemark');
   late Position _currentPosition;
   final animation =
-  const MapAnimation(type: MapAnimationType.smooth, duration: 2.0);
+      const MapAnimation(type: MapAnimationType.smooth, duration: 2.0);
   double zoomLevel = 2;
   final MapObjectId mapObjectId = const MapObjectId('normal_icon_placemark');
 
@@ -41,23 +76,26 @@ class __MapScreenStateState extends State<_MapScreenState> {
       return;
     }
 
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.deniedForever) {
-      // Обработка случая, когда пользователь навсегда отклонил доступ к геолокации
-      print('КИНА НЕ БУДЕТ');
-      return;
-    }
-
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission != LocationPermission.whileInUse &&
-          permission != LocationPermission.always) {
-        // Обработка случая, когда пользователь отказал в доступе к геолокации
-        print('ПРОБКИ ВЫБИЛО');
+    try {
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.deniedForever) {
+        // Обработка случая, когда пользователь навсегда отклонил доступ к геолокации
+        print('КИНА НЕ БУДЕТ');
         return;
       }
-    }
 
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission != LocationPermission.whileInUse &&
+            permission != LocationPermission.always) {
+          // Обработка случая, когда пользователь отказал в доступе к геолокации
+          print('ПРОБКИ ВЫБИЛО');
+          return;
+        }
+      }
+    } catch (e) {
+      print('Ошибка при запросе разрешений: $e');
+    }
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.high);
@@ -68,13 +106,20 @@ class __MapScreenStateState extends State<_MapScreenState> {
             longitude: _currentPosition.longitude));
       });
     } catch (e) {
-      print(e);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error occurred: $e'),
+        ),
+      );
     }
   }
 
   void _onMapTap(Point point) {
-    print('Нажатие по карте: $point');
-    // Можно продумать дальнейшую логику, но это сложно(((
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Нажатие по карте: $point'),
+      ),
+    );
   }
 
   @override
@@ -141,15 +186,14 @@ class __MapScreenStateState extends State<_MapScreenState> {
                 direction: 90,
                 isDraggable: true,
                 icon: PlacemarkIcon.single(PlacemarkIconStyle(
-                    image:
-                    BitmapDescriptor.fromAssetImage('lib/assets/route_start.png'),
-                    rotationType: RotationType.noRotation )),
+                    image: BitmapDescriptor.fromAssetImage(
+                        'lib/assets/route_start.png'),
+                    rotationType: RotationType.noRotation)),
               );
 
               setState(() {
-                mapObjects.add(mapObject);
+                mapObjects.add(mapObject as MapObject);
               });
-
             },
             child: Icon(Icons.my_location),
           ),
